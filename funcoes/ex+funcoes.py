b4384ade2022f1f3b9e32bf13dadebf3631c2fc8
Rodrigo Kunzler Weber <rkweber05@outlook.com>
2024-09-11 17:53:44 -0300

    Variaveis, tipos de dados e funções

---
+++ funcoes/ex+funcoes.py
@@ -0,0 +1,16 @@
+"""
+    Nome: Rodrigo
+    Função: carimbar contratos
+    pega um contrato => carimba o contrato => devolve um contrato
+
+    f(x) = 2.x => y
+    f(10) = 2.10 => 20
+"""
+from traceback import print_tb
+
+
+def multiplica(numero, multiplicador):
+    resultado = numero * multiplicador
+    return resultado
+
+print(multiplica(10, 3))
---
+++ variaveis_tipos_de_dados/tipo_de_dados.py
@@ -0,0 +1,21 @@
+# Tipos primitivos
+# (valor, endereço)
+numero = 900 # int ou inteiro
+numero_decimal = 3.99 # float ou ponto flutuante
+maior_de_idade = False # false ou true
+
+# Tipos Compostos
+# (quais os endereços que contém os dados da lista, enderço)
+listas_de_numeros = [1,2,3]
+lista_de_nomes = ["João", "Jose"]
+lista_de_floats = [1.33, 1.32]
+
+# interadores
+
+for numero in listas_de_numeros:
+    print(numero)
+
+text = "Rodrigo Kunzler Weber, 19 anos, texto"
+
+for letra in text:
+    print(letra)
